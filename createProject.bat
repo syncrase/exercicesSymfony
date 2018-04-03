b9900df8f7d6b2d55b6aa081f0528a28ea01ec5f
syncrase <pierre.taquet@gmail.com>
2018-04-03 15:26:05 +0200

    Utilisation basique de vis.js

--- createProject.bat
+++ createProject.bat
@@ -1,4 +1,5 @@
-PROJECT_FOLDER=files
+:: This file doesn't work but this is the right command sequence
+set PROJECT_FOLDER=chronologie
 composer create-project symfony/skeleton %PROJECT_FOLDER%
 cd %PROJECT_FOLDER%
 composer require profiler
